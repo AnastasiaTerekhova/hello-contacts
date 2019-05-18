146ca309ac8b975c297468ac9e17ce2422996913
AnastasiaTerekhova <AnastasiaTerekhova@users.noreply.github.com>
2019-05-18 09:26:34 +0300

    config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-module.export = {
-    publickPath: process.env.NODE_ENV === 'production' 
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' 
     ? '/hello-contacts/' 
     : '/' 
     }
